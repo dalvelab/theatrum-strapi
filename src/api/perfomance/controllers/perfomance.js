231dfb26bcd2f83665171811f2a16923888daedb
dalvelab <dalvelab@gmail.com>
2023-04-28 18:56:24 +0500

    add main API instances

---
+++ src/api/perfomance/controllers/perfomance.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * perfomance controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::perfomance.perfomance');
---
+++ src/api/perfomance/routes/perfomance.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * perfomance router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::perfomance.perfomance');
