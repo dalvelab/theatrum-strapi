a990bca1eb61b4bf80f6650498b315f7cd2b9d03
dalvelab <dalvelab@gmail.com>
2023-05-02 11:20:40 +0500

    add message collection and date to post slug

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -6,20 +6,24 @@ module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
     if (data.title) {
-      const title = data.title.replace(/,/gi, '');
+      const newDate = new Date();
+
+      const title = data.title.replace(/,/gi, '').replace(/_/g, ' ');
+      const date = `${String(newDate.getDate()).padStart(2, '0')}-${String(newDate.getMonth()).padStart(2, '0')}-${newDate.getFullYear()}`;
 
       data.slug = cyrillicToTranslit
-        .transform(title, "-")
+        .transform(`${title}_${date}`, "-")
         .toLowerCase();
     }
   },
-  beforeUpdate(event) {
+  beforeUpdate(event) { 
     const { data } = event.params;
     if (data.title) {
-      const title = data.title.replace(/,/gi, '');
+      const created_at = data.slug.split('_')[1]; 
+      const title = data.title.replace(/,/gi, '').replace(/_/g, ' ');
 
       data.slug = cyrillicToTranslit
-        .transform(title, "-")
+        .transform(`${title}_${created_at}`, "-")
         .toLowerCase();
     }
   },
