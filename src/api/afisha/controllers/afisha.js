214e70e6e9842d19741016b027224a696bfbaaa5
dalvelab <dalvelab@gmail.com>
2023-05-20 12:17:43 +0500

    add sorting to afisha

--- src/api/afisha/controllers/afisha.js
+++ src/api/afisha/controllers/afisha.js
@@ -6,4 +6,33 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::afisha.afisha');
+module.exports = createCoreController('api::afisha.afisha', ({strapi}) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    if (!data || data.length === 0) {
+      return { data, meta };
+    }
+
+    const sortedData = data.map((afisha) => {
+      if (!afisha.attributes.tickets || afisha.attributes.tickets.length < 2) {
+        return afisha;
+      }
+
+      const sortTickets = afisha.attributes.tickets.sort((a, b) => new Date(a.date) - new Date(b.date))
+      
+      afisha.attributes.tickets = sortTickets;
+      
+      return afisha;
+    })
+    .sort((a, b) => {
+      if (!a.attributes.tickets) {
+        return;
+      }
+
+      return new Date(a.attributes.tickets[0].date) - new Date(b.attributes.tickets[0].date)
+    });
+
+    return { data: sortedData, meta };
+  },
+}));
