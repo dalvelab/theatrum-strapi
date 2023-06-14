0dc9c6791dec8ab0ad9964486bcae8fae0a16b74
dalvelab <dalvelab@gmail.com>
2023-06-14 19:39:32 +0500

    add cron job to delete past events

---
+++ config/cron-tasks.js
@@ -0,0 +1,39 @@
+module.exports = {
+  deletePastEventsFromAfisha: {
+    task: async ({ strapi }) => {
+      try {
+        const afisha = await strapi.entityService.findMany("api::afisha.afisha", {
+          populate: {
+            tickets: {
+              fields: ['date'],
+              sort: 'date:asc'
+            }
+          }
+        });
+
+        const currentDate = new Date();
+        
+        afisha.forEach((event) => {
+          if (new Date(event.tickets[0].date) < currentDate && event.tickets.length === 1) {
+            strapi.entityService.delete("api::afisha.afisha", event.id);
+          }
+
+          if (new Date(event.tickets[0].date) < currentDate && event.tickets.length > 1) {
+            const tickets = event.tickets.filter((ticket) => new Date(ticket.date) > currentDate);
+
+            strapi.entityService.update("api::afisha.afisha", event.id, {
+              data: {
+                tickets,
+              }
+            })
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    options: {
+      rule: "* 22 * * *",
+    },
+  },
+};
--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
+const cronTasks = require("./cron-tasks");
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  cron: {
+    enabled: env.bool("CRON_ENABLED", true),
+    tasks: cronTasks,
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  app: {
+    keys: env.array("APP_KEYS"),
   },
 });
