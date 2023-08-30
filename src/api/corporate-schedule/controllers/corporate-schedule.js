5b1715d136053c768acbdca85d8bc369c5201684
dalvelab <dalvelab@gmail.com>
2023-08-30 21:21:05 +0500

    add sorting to corporate-schedule FIND controller

--- src/api/corporate-schedule/controllers/corporate-schedule.js
+++ src/api/corporate-schedule/controllers/corporate-schedule.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::corporate-schedule.corporate-schedule');
+module.exports = createCoreController('api::corporate-schedule.corporate-schedule', ({strapi}) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    if (!data || data.length === 0) {
+      return { data, meta };
+    }
+
+    const sortedData = data.sort((a, b) => {
+      return new Date(a.attributes.date) - new Date(b.attributes.date)
+    });
+
+    return { data: sortedData, meta };
+  },
+}));
+
