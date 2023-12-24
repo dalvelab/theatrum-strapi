784c9d78369ea092ea67118275379ec998c533ea
dalvelab <dalvelab@gmail.com>
2023-12-24 22:47:24 +0500

    update corporate-schedule-archive controller to optimise archive in worker app

--- src/api/corporate-schedule-archive/controllers/corporate-schedule-archive.js
+++ src/api/corporate-schedule-archive/controllers/corporate-schedule-archive.js
@@ -7,17 +7,45 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::corporate-schedule-archive.corporate-schedule-archive', ({strapi}) => ({
-  async find(ctx) {
-    const { data, meta } = await super.find(ctx);
-
-    if (!data || data.length === 0) {
-      return { data, meta };
-    }
-
-    const sortedData = data.sort((a, b) => {
-      return new Date(b.attributes.date) - new Date(a.attributes.date)
+  async find() {
+    const data = await strapi.entityService.findMany("api::corporate-schedule-archive.corporate-schedule-archive", {
+      sort: {
+        date: 'desc'
+      }
     });
 
-    return { data: sortedData, meta };
+    if (!data || data.length === 0) {
+      return {
+        data: []
+      };
+    }
+
+    const uniqueMonthsSet = new Set(data.map((event) => `${new Date(event.date).getMonth() + 1}-${new Date(event.date).getFullYear()}`));
+
+    const uniqueMonths = [];
+
+    uniqueMonthsSet.forEach((el) => uniqueMonths.push(el));
+
+    return { data: uniqueMonths };
   },
+    async findOne(ctx) {
+    const id = ctx.params.id;
+
+    const month = id.split('-')[0];
+    const year = id.split('-')[1];
+
+    const data = await strapi.entityService.findMany("api::corporate-schedule-archive.corporate-schedule-archive", {
+      filters: {
+        date: {
+          $gte: new Date(`${year}-${month}-01`),
+          $lte: new Date(`${year}-${month}-${new Date(year, month, 0).getDate()}`)
+        }
+      },
+      sort: {
+        date: 'desc'
+      }
+    });
+
+    return { data };
+  }
 }));
